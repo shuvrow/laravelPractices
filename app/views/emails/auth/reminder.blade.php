543be4147e17435b16f9cac288214761a2f4f43a
shuvrow <shuvrow@gmail.com>
2015-04-02 07:38:04 -0400

    test

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -9,6 +9,8 @@
 		<div>
 			To reset your password, complete this form: {{ URL::to('password/reset', array($token)) }}.<br/>
 			This link will expire in {{ Config::get('auth.reminder.expire', 60) }} minutes.
+
+			ok please change this part while merging!!!! is it done??
 		</div>
 	</body>
 </html>
